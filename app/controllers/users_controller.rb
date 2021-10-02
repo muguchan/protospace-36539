53d4f3aaa8a5dcb2629e1bf5af9461b516e17d92
muguchan <naoko.unsui@gmail.com>
2021-10-02 16:15:54 +0900

    修正しました３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,5 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) 
-    @name = current_user.name
-    @profile = current_user.profile
-    @occupation = current_user.occupation
-    @position = current_user.position
-    @prototype = Prototype.find(params[:id])
   end
 end
