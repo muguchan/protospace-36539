c22b1a04ad49d7dd478aabef3890c148946d9c10
muguchan <naoko.unsui@gmail.com>
2021-10-02 13:38:00 +0900

    追加コミット

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -49,6 +50,13 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user == @prototype.user
+      redirect_to action: :index
+    end
   end
 end
 
